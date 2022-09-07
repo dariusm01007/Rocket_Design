cf1b9fe5ce225391ed5aff81c0eb6c7568cb7088
dariusm01 <dariusmensah01@outlook.com>
2022-09-06 18:34:21 -0700

    Added function descriptions

--- Sims/Python/IMU_Sim_9_3_2022/mathUtilities.py
+++ Sims/Python/IMU_Sim_9_3_2022/mathUtilities.py
@@ -2,11 +2,40 @@ import numpy as np
 import math
 from numpy.linalg import matrix_power
 
+###########################################################
+#   Function    : deg2rad()
+#
+#   Description : Converts angles from degrees to radians
+#
+#   Example call: angle = deg2rad(25)
+############################################################
+
 def deg2rad(angle):
     return angle * (np.pi/180.0)
 
+
+
+###########################################################
+#   Function    : rad2deg()
+#
+#   Description : Converts angles from radians to degrees
+#
+#   Example call: angle = rad2deg(0.4363)
+############################################################
+
 def rad2deg(angle):
     return angle* (180.0/np.pi)
+
+
+
+###########################################################
+#   Function    : rotMat()
+#
+#   Description : Performs coordinate frame transformation
+#                 the input angle is intented to be in radians
+#
+#   Example call: R = rotMat(0.4363, 'z')
+############################################################
 
 def rotMat(angle, axis):
     if axis == 'x' or axis == 'X':
@@ -30,12 +59,49 @@ def rotMat(angle, axis):
     return R
 
 
+
+###########################################################
+#   Function    : Inertial2Body()
+#
+#   Description : Performs a series of coordinate frame 
+#                 transformations to convert from the inertial
+#                 frame to the body frame. The input angles are
+#                 intented to be in radians
+#
+#   Example call: R  = Inertial2Body(0.0873, 0.3840, -1.5533)
+############################################################
+
 def Inertial2Body(roll, pitch, yaw):
     # The @ means 'matrix multiply'
     return rotMat(roll,'x')@(rotMat(pitch, 'y')@rotMat(yaw, 'z'))
-    
+
+
+
+###########################################################
+#   Function    : Body2Inertial()
+#
+#   Description : Performs a series of coordinate frame 
+#                 transformations to convert from the body
+#                 frame to the inertial frame. The input angles are
+#                 intented to be in radians
+#
+#   Example call: R  = Body2Inertial(0.0873, 0.3840, -1.5533)
+############################################################  
+  
 def Body2Inertial (roll, pitch, yaw):
     return np.transpose(Inertial2Body(roll, pitch, yaw))
+
+
+###########################################################
+#   Function    : extractEuler()
+#
+#   Description : Given an inertial to body frame transformation
+#                 matrix, the Euler angles can be extracted using
+#                 inverse trig functions on the values at certain 
+#                 positions on the matrix
+#
+#   Example call: angles = extractEuler(matrix)
+############################################################ 
 
 def extractEuler(R):
     # Extracting Euler angles from Inertial2Body(roll, pitch, yaw)
@@ -47,12 +113,29 @@ def extractEuler(R):
                              [theta],
                              [psi]]))
 
+
+###########################################################
+#   Function    : EulerKinematic()
+#
+#   Description : Using Euler's Kinematical equations, the
+#                 attitude rate from the dynamics can be 
+#                 converted into attitude rates (roll rate,
+#                 pitch rate, yaw rate). However if the 
+#                 pitch angle reaches 90 degrees, there is
+#                 a division by zero error
+#
+#                 Input angles should be an array size = 2
+#                 Body rates should be a 3x1 column vector
+#
+#   Example call: angleRate = EulerKinematic(angles, BodyRates)
+############################################################ 
+
 def EulerKinematic(angles, BodyRate):
     
     roll  = angles[0].item()
     pitch = angles[1].item()
     
-    # cos(pitch) == 90 -> Gimbal Lock
+    # pitch == 90 -> Gimbal Lock
     # 1/cos(90) = undefined
     
     R = np.array([[1, np.sin(roll)*np.tan(pitch), np.cos(roll)*np.tan(pitch)], 
@@ -63,6 +146,18 @@ def EulerKinematic(angles, BodyRate):
     # The @ means 'matrix multiply'
     return R@BodyRate
 
+
+###########################################################
+#   Function    : IMU_AttitudeUpdate()
+#
+#   Description : Rather than using Euler's kinematical 
+#                 equations, another way of extracting 
+#                 Euler angles is by calculating the rate
+#                 of the DCM, integrating, and extracting 
+#                 the angles similar to extractEuler()
+#
+#   Example call: angles = IMU_AttitudeUpdate(dcmOLD, Gyro, dt)
+############################################################ 
 
 def IMU_AttitudeUpdate(dcmOLD, Gyro, dt):
     
@@ -93,6 +188,18 @@ def IMU_AttitudeUpdate(dcmOLD, Gyro, dt):
     return rad2deg(np.array([[phi],
                              [theta],
                              [psi]]))
+
+###########################################################
+#   Function    : tiltAngles()
+#
+#   Description : Euler angles (roll & pitch) can be found
+#                 by measuring acceleration relative to gravity
+#
+#                 Input angles acceleration should be a 3x1
+#                 column vector
+#
+#   Example call: tiltAngles(accel)
+############################################################ 
 
 def tiltAngles(accel):
     
